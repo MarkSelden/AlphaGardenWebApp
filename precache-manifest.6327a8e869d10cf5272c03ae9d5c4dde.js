e78af0477264fb2ab5563c0a0231781357082f92
MarkSelden <seldenm@berkeley.edu>
2020-01-17 11:07:35 -0800

    Updates

---
+++ precache-manifest.6327a8e869d10cf5272c03ae9d5c4dde.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df93c6450fa17499ceaeaf7193a2dc67",
+    "url": "/AlphaGardenWebApp/index.html"
+  },
+  {
+    "revision": "3ae1a48d1271bd02b472",
+    "url": "/AlphaGardenWebApp/static/css/main.166e5979.chunk.css"
+  },
+  {
+    "revision": "c89f646758d4784a176f",
+    "url": "/AlphaGardenWebApp/static/js/2.f35024f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/AlphaGardenWebApp/static/js/2.f35024f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ae1a48d1271bd02b472",
+    "url": "/AlphaGardenWebApp/static/js/main.cd8d25ad.chunk.js"
+  },
+  {
+    "revision": "0a16bf459cc99278a520",
+    "url": "/AlphaGardenWebApp/static/js/runtime-main.0c451884.js"
+  },
+  {
+    "revision": "2eca6955beead79f6273d5be0822c1ac",
+    "url": "/AlphaGardenWebApp/static/media/Garden-Overview.2eca6955.bmp"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AlphaGardenWebApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d55bddf8d62910879ed9f605522149a8",
+    "url": "/AlphaGardenWebApp/static/media/testSmall.d55bddf8.mp4"
+  }
+]);
