8005442252177a5e311ddc0b4d352531c417f78a
MarkSelden <seldenm@berkeley.edu>
2020-01-27 14:49:08 -0800

    Element 2 styling

--- alpha-garden/src/App.js
+++ alpha-garden/src/App.js
@@ -38,6 +38,8 @@ class App extends React.Component {
 
       <div>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto+Mono"/>
         
         {this.state.page}
         
