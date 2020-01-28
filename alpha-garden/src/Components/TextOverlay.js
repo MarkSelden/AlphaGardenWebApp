0f4c7ec2c66e22636d67834db7d03a51309390d7
MarkSelden <seldenm@berkeley.edu>
2020-01-28 15:00:17 -0800

    trying something here

--- alpha-garden/src/Components/TextOverlay.js
+++ alpha-garden/src/Components/TextOverlay.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import GlowingMarks from './GlowingMarks'
 	
 	let diversity = "36%";
+	let growth = "50%";
 	let plants = ["Lavendar", "Basil", "Turnips", "Rosemary", "Cacti"];
 	const plantMap = (x) => {return x + ", "}
  //component for the text that goes over the background video
  const TextOverLay = (props) => {
 
+ 	if(props.nuc){setTimeout(props.endFunc, 6000)}
+
 	let today = new Date();
 	 return(
  	 	<div className = "LOADING">
+ 	 		<GlowingMarks />
 	 		<img src={require("./Garden-Overview.png")} alt="Zaaa GARDEN" height="100%" width="100%" />
-	 		<p className="top"> LOADING ALPHA GARDEN... </p>
+
 	    	<div className="Overlay">
+	    		<p className="top"> ALPHA GARDEN</p>
 		 		<button id="button" onClick={props.endFunc}> Day  {today.getDate() + today.getMonth()} </button>
 		 	</div>
 		 	<div className = "Data">
-		 			<p> Diversity: {diversity} + <br/> Plants: {plants.map(plantMap)} </p>
+		 			<p> Growth: +{growth} <br/> Diversity: +{diversity} </p>
 		 	</div>
 		</div>
 		)    
